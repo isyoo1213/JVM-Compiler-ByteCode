84a9fd100260cdd3bf094655d95bb376af5cfaf4
isyoo1213 <moethns@gmail.com>
2023-04-21 21:55:45 +0900

    feat: Add class/disassemble for InnerClass

---
+++ javap/src/main/sourcecode/innerclass/MyLinkedListTest.java
@@ -0,0 +1,13 @@
+package main.sourcecode.innerclass;
+
+public class MyLinkedListTest {
+    public static void main(String[] args) {
+        MyLinkedList myList = new MyLinkedList();
+        myList.print();
+
+        myList.add("JAVA");
+        myList.add("SRPING");
+        myList.add("JPA");
+        myList.print();
+    }
+}
