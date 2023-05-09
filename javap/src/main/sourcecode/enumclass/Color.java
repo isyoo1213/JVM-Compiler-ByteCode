4ce74196099b8954de8082ab670179d4e2b4177c
isyoo1213 <moethns@gmail.com>
2023-05-09 21:51:36 +0900

    ** for private - example for enum

---
+++ javap/src/main/sourcecode/enumclass/Color.java
@@ -0,0 +1,7 @@
+package main.sourcecode.enumclass;
+
+public class Color {
+    public static final Color RED = new Color();
+    public static final Color BLUE = new Color();
+    public static final Color GREEN = new Color();
+}
