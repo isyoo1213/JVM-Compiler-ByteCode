0f3277d95d07439aac21e1d19afdde9dcd97f4de
isyoo1213 <moethns@gmail.com>
2023-05-13 23:24:24 +0900

    ** for private - Ex for static final field

--- javap/src/main/sourcecode/enumclass/Color.java
+++ javap/src/main/sourcecode/enumclass/Color.java
@@ -2,6 +2,9 @@ package main.sourcecode.enumclass;
 
 public class Color {
     public static final Color RED = new Color();
+    public Color nonStaticRED = new Color();
     public static final Color BLUE = new Color();
+    public Color nonStaticBLUE = new Color();
     public static final Color GREEN = new Color();
+    public Color nonStaticGREEN = new Color();
 }
