fcc85d8649fd83d9e2a789fd1480d5b1808eb0a5
isyoo1213 <moethns@gmail.com>
2023-05-27 02:03:53 +0900

    ** private - CircularDoublyLinkedList - search/add

---
+++ javap/src/main/sourcecode/list/MyCircularDoublyLinkedList.java
@@ -0,0 +1,118 @@
+package main.sourcecode.list;
+
+public class MyCircularDoublyLinkedList<E> {
+    private Node<E> head;
+    private Node<E> tail;
+    private int size;
+
+    public MyCircularDoublyLinkedList() {
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
+
+    public Node<E> search(int index) {
+        if (head == null) {
+            throw new IndexOutOfBoundsException("리스트가 비어있습니다.");
+        }
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        Node<E> n = head;
+        for (int i = 0; i < index; i++) {
+            n = n.next;
+        }
+        return n;
+    }
+
+    public Node<E> efficientSearch(int index) {
+        if (head == null) {
+            throw new IndexOutOfBoundsException("리스트가 비어있습니다.");
+        }
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        Node<E> n = head;
+        if ((size / 2) >= index) {
+            for (int i = 0; i < index; i++) {
+                n = n.next;
+            }
+        } else {
+            for (int i = size - 1; i > index; i--) {
+                n = n.prev;
+            }
+        }
+        return n;
+    }
+
+    public void addFirst(E value) {
+        Node<E> first = head;
+        // tail    head -> next ... tail
+        Node<E> newNode = new Node<E>(null, value, first);
+        //우선 circular에 대한 처리는 나중에 처리하는 것으로 가정하고 prev노드는 null로 생성
+        head = newNode;
+        size++;
+
+        // List에 자료가 없을 경우
+        if (first == null || tail == null) {
+            tail = newNode;
+            //circular 처리
+            // 1. head -> next
+            // 2. tail -> head
+            newNode.next = head;
+            // 3. head -> tail
+            // 4. tail
+            newNode.prev = head;
+            //tail.next = head;
+            //어차피 head/tail이 모두 newNode를 가리키므로 head/tail로 구분해서 처리할 필요가 없다
+        }
+
+        // List에 자료가 존재할 경우 circular 처리
+        //tail -> head    next ... tail
+        tail.next = head;
+        //tail <- head    next ... tail
+        head.prev = tail;
+        //tail    head <- next ... tail
+        first.prev = head;
+    }
+
+    public void addLast(E value) {
+        Node<E> last = tail;
+        //tail    head ... prev <- tail
+        Node<E> newNode = new Node<>(last, value, null);
+        //우선 circular에 대한 처리는 나중에 처리하는 것으로 가정하고 next노드는 null로 생성
+        tail = newNode;
+
+        // List에 자료가 없을 때 -> last가 null이므로 last의 필드를 사용하는 못하는 것에 대한 처리
+        if (head == null || last == null) {
+            head = newNode;
+            //circular 처리
+            //2. tail    head <- tail
+            //1. tail <- head    tail
+            newNode.prev = newNode;
+            //3. tail -> head    tail
+            //4. tail    head -> tail
+            newNode.next = newNode;
+        }
+
+        // tail -> head ... prev    tail
+        tail.next = head;
+        // tail <- head ... prev    tail
+        head.prev = tail;
+        // tail    head ... prev -> tail
+        last.next = tail;
+    }
+
+    private static class Node<E>{
+        private E data;
+        private Node<E> prev;
+        private Node<E> next;
+
+        public Node( Node<E> prev, E data, Node<E> next) {
+            this.data = data;
+            this.prev = prev;
+            this.next = next;
+        }
+    }
+}
