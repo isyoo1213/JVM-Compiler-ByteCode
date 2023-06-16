502221c8184c0d729d484201515c512a446a5de0
isyoo1213 <moethns@gmail.com>
2023-06-16 23:40:37 +0900

    ** private - HashSet - add/resize

--- javap/src/main/sourcecode/set/MyHashSet.java
+++ javap/src/main/sourcecode/set/MyHashSet.java
@@ -24,9 +24,65 @@ public class MyHashSet<E> implements MySet<E> {
         }
     }
 
+    @SuppressWarnings("unchecked")
+    private void resize() {
+        int newCapacity = table.length * 2;
+        final Node<E>[] newTable = (Node<E>[]) new Node[newCapacity];
+        for (int i = 0; i < table.length; i++) {
+            Node<E> value = table[i];
+            if (value == null) {
+                continue;
+            }
+            table[i] = null;
+            Node<E> nextNode;
+            while (value != null) {
+                int index = value.hash % newCapacity;
+                if (newTable[index] != null) {
+                    Node<E> tail = newTable[index];
+                    while (tail.next != null) {
+                        tail = tail.next;
+                    }
+                    nextNode = value.next;
+                    value.next = null;
+                    tail.next = value;
+                } else {
+                    nextNode = value.next;
+                    value.next = null;
+                    newTable[index] = value;
+                }
+                value = nextNode;
+            }
+        }
+        table = null;
+        table = newTable;
+    }
+
+    private E add(int hash, E key) {
+        int index = hash % table.length;
+        if (table[index] == null) {
+            table[index] = new Node<E>(hash, key, null);
+        } else {
+            Node<E> temp = table[index];
+            Node<E> prev = null;
+            while (temp != null) {
+                if ((temp.hash == hash) && (temp.key == key || temp.key.equals(key))) {
+                    return key;
+                }
+                prev = temp;
+                temp = temp.next;
+            }
+            prev.next = new Node<E>(hash, key, null);
+        }
+        size++;
+        if (size >= LOAD_FACTOR * table.length) {
+            resize();
+        }
+        return null;
+    }
+
     @Override
     public boolean add(E e) {
-        return false;
+        return add(hash(e), e) == null;
     }
 
     @Override
