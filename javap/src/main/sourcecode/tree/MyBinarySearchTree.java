98c23453c24f5c97ddf7766ce13f02ef3b0540fb
isyoo1213 <moethns@gmail.com>
2023-06-24 22:02:51 +0900

    ** private - BinarySearchTree - Node/basicSettings

---
+++ javap/src/main/sourcecode/tree/MyBinarySearchTree.java
@@ -0,0 +1,64 @@
+package main.sourcecode.tree;
+
+import java.util.Comparator;
+
+public class MyBinarySearchTree<E> {
+
+    private Node<E> root;
+    private int size;
+
+    private final Comparator<? super E> comparator;
+
+    public MyBinarySearchTree() {
+        this(null);
+    }
+
+    public MyBinarySearchTree(Comparator<? super E> comparator) {
+        this.comparator = comparator;
+        this.root = null;
+        this.size = 0;
+    }
+
+    public boolean add(E value) {
+        if (comparator == null) {
+            return addUsingComparable(value) == null;
+        }
+        //return addusingComparator(value, this.comparator) == null; //아직 미구현
+    }
+
+    private E addUsingComparable(E value) {
+        Node<E> current = root;
+
+        if (current == null) {
+            root = new Node<E>(value);
+            size++;
+            return null;
+        }
+
+        Comparable<? super E> compValue = (Comparable<? super E>) value;
+        int compResult;
+        Node<E> currentParent; //다음 순회를 위한 변수
+
+        do {
+            currentParent = current;
+            compResult = compValue.compareTo(current.value);
+            if (compResult < 0) {
+                current = current.left;
+            } else if (compResult > 0) {
+                current = current.right;
+            } else {
+                return value;
+            }
+        } while (current != null);
+        //순회 완료 - 즉, 우선적으로 null 유무를 떠나서 left/right 중 value가 들어갈 위치가 결정됨
+
+        Node<E> newNode = new Node<E>(value, currentParent);
+        if (compResult < 0) {
+            current.left = newNode;
+        } else {
+            current.right = newNode;
+        }
+        size++;
+        return null;
+    }
+}
