248d9a93067c46a34d1bcba316b23f4284954830
isyoo1213 <moethns@gmail.com>
2023-06-26 22:59:48 +0900

    ** private - BinarySearchTree - deleteNode/getSuccessorAndUnlink

--- javap/src/main/sourcecode/tree/MyBinarySearchTree.java
+++ javap/src/main/sourcecode/tree/MyBinarySearchTree.java
@@ -94,4 +94,69 @@ public class MyBinarySearchTree<E> {
         size++;
         return null;
     }
+
+    private Node<E> getSuccessorAndUnlink(Node<E> node) {
+        Node<E> currentParent = node;
+        Node<E> current = node.right;
+        if (current.left == null) {
+            currentParent.right = current.right;
+            if (currentParent.right != null) {
+                currentParent.right.parent = currentParent;
+            }
+            current.right = null;
+            return current;
+        }
+
+        while (current.left != null) {
+            currentParent = current; //대체할 노드의 오른쪽 자식 노드의 유무를 확인 후 *** 대체할 노드의 자리에 올려주기 위해 탐색노드의 부모노드를 계속 유지
+            current = current.left;
+        }
+
+        currentParent.left = current.right; //대체할 노드의 부모에서, 대체할 노드의 오른쪽 자식 노드를 연결
+        if (currentParent.left != null) {
+            currentParent.left.parent = currentParent; // 대체할 노드의 오른쪽 자식 노드가 null이 아니라면, 대체할 노드의 부모노드를 향한 방향으로도 parent로 연결
+        }
+
+        current.right = null; //대체할 노드에서 오른쪽 자식 노드의 연결을 해제
+        return current;
+    }
+
+    private Node<E> deleteNode(Node<E> node) {
+        if (node != null) {
+            //자식 노드가 없을 경우
+            if (node.left == null && node.right == null) {
+                if (node == root) {
+                    root = null;
+                } else {
+                    node = null;
+                }
+                return null;
+            }
+            //자식 노두가 모두 있을 경우
+            if (node.left != null && node.right != null) {
+                Node<E> relpaceNode = getSuccessorAndUnlink(node);
+                node.value = relpaceNode.value;
+            //왼쪽 노드만 존재하는 경우
+            } else if (node.left != null) {
+                if (node == root) {
+                    node = node.left;
+                    root = node;
+                    root.parent = null;
+                } else {
+                    node = node.left;
+                }
+            //오른쪽 노드만 존재하는 경우
+            } else {
+                if (node == root) {
+                    node = node.right;
+                    root = node;
+                    root.parent = null;
+                } else {
+                    node = node.right;
+                }
+            }
+        }
+        return node;
+    }
+
 }
