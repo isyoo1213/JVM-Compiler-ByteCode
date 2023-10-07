bd65baea4f52d03151393435ec0a3084d0fd9c59
isyoo1213 <moethns@gmail.com>
2023-10-07 10:22:10 +0900

    feat: Detach from 'mytest' directory

---
+++ javap/src/main/sourcecode/staticPractice/FieldWIthConstructor.java
@@ -0,0 +1,30 @@
+package main.sourcecode.staticPractice;
+
+public class FieldWIthConstructor {
+    public int age;
+    public String name;
+    public static int publicAge;
+    public static String publicName;
+    public static final int androidAge = 5;
+    public static final String androidName = "and_azma";
+
+    public ForStaticVar noneStaticVar = new ForStaticVar();
+    public static ForStaticVar staticVar = new ForStaticVar();
+
+    public FieldWIthConstructor() {
+        this.age = 10;
+        this.name = "koala";
+        this.publicAge = 3;
+        publicName = "azma";
+    }
+
+    public void useNonStaticField() {
+        noneStaticVar.print();
+    }
+
+    public void useStaticField() {
+        System.out.println(staticVar);
+        staticVar.print();
+    }
+
+}
---
+++ javap/src/main/sourcecode/staticPractice/ForStaticVar.java
@@ -0,0 +1,11 @@
+package main.sourcecode.staticPractice;
+
+public class ForStaticVar {
+    public int age = 5;
+    public String name = "Koala";
+
+    public void print() {
+        System.out.println(age);
+        System.out.println(name);
+    }
+}
